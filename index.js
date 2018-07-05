69fe616e5987b2cc349355c2f41581273b40c6a8
szabolcsit <szabolcsit@hu.ibm.com>
2018-07-05 13:18:58 +0200

    update deps

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+if(process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ApolloEngine } = require('apollo-engine')
